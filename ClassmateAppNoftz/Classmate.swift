9471ffedc58d77731f660818a3319465af9b4ef5
Evangeline Noftz <esnoftz@gmail.com>
2024-10-02 08:08:59 -0500

    Initial commit.

---
+++ ClassmateAppNoftz/Classmate.swift
@@ -0,0 +1,28 @@
+//
+//  Classmate.swift
+//  ClassmateAppNoftz
+//
+//  Created by EVANGELINE NOFTZ on 10/2/24.
+//
+
+import Foundation
+
+class Classmate {
+    
+    // Variables
+    var realName: String
+    var grade: Int
+    var boat: Int
+    var nickname: String
+    
+    init(realName: String, grade: Int, boat: Int, nickname: String) {
+        self.realName = realName
+        self.grade = grade
+        self.boat = boat
+        self.nickname = nickname
+    }
+    
+    
+    
+    
+}
