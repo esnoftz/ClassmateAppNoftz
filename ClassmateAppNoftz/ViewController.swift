96b16b02f62f2bdcdd8ae2ef03ed88790d59cfe2
Evangeline Noftz <esnoftz@gmail.com>
2024-10-02 07:16:10 -0500

    Initial Commit

---
+++ ClassmateAppNoftz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassmateAppNoftz
+//
+//  Created by EVANGELINE NOFTZ on 10/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
