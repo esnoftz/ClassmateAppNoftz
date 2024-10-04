ea77734c4d7b5922f362231cb9e83e5afa4b1cd1
Evangeline Noftz <esnoftz@gmail.com>
2024-10-04 08:05:08 -0500

    Worked.

--- ClassmateAppNoftz/SeeStudentsViewController.swift
+++ ClassmateAppNoftz/SeeStudentsViewController.swift
@@ -9,13 +9,51 @@ import UIKit
 
 class SeeStudentsViewController: UIViewController {
 
+    var numStudent = 0
+    
+    @IBOutlet weak var studentInfoTextView: UITextView!
+    
+    @IBOutlet weak var realNameInput: UITextField!
+    
+    @IBOutlet weak var nicknameInput: UITextField!
+    
+    @IBOutlet weak var gradeInput: UITextField!
+    
+    @IBOutlet weak var boatInput: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
     }
     
+    
+    @IBAction func nextStudentAction(_ sender: UIButton) {
+        
+        studentInfoTextView.text = "Student: \(AppData.classmates[numStudent].realName)\nNickname: \(AppData.classmates[numStudent].nickname)\nGrade: \(AppData.classmates[numStudent].grade)\nBoat: \(AppData.classmates[numStudent].boat)"
+        numStudent += 1
+        if numStudent == AppData.classmates.count {
+            numStudent = 0
+        }
 
+    }
+    
+    
+    @IBAction func sortStudentsAction(_ sender: UIButton) {
+        
+        AppData.classmatesNames.sort()
+        for classmate in AppData.classmatesNames {
+            studentInfoTextView.text += "\n\(classmate)"
+        }
+        
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- ClassmateAppNoftz/ViewController.swift
+++ ClassmateAppNoftz/ViewController.swift
@@ -5,37 +5,46 @@
 //  Created by EVANGELINE NOFTZ on 10/2/24.
 //
 
+class AppData {
+    static var classmates = [Classmate]()
+    static var classmatesNames = [String]()
+}
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
-    var classmates = [Classmate]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        classmates.append(Classmate(realName: "Eva Noftz", grade: 12, boat: 6, nickname: "No Pickles Noftz"))
-        classmates.append(Classmate(realName: "Ava Schmidt", grade: 12, boat: 6, nickname: "ABBA Ava"))
-        classmates.append(Classmate(realName: "Evan Meyer", grade: 11, boat: 6, nickname: "Flutin Evan"))
-        classmates.append(Classmate(realName: "Justin Weber", grade: 12, boat: 6, nickname: "Wicked Weber"))
+        AppData.classmates.append(Classmate(realName: "Eva Noftz", grade: 12, boat: 6, nickname: "No Pickles Noftz"))
+        AppData.classmates.append(Classmate(realName: "Ava Schmidt", grade: 12, boat: 6, nickname: "ABBA Ava"))
+        AppData.classmates.append(Classmate(realName: "Evan Meyer", grade: 11, boat: 6, nickname: "Flutin Evan"))
+        AppData.classmates.append(Classmate(realName: "Justin Weber", grade: 12, boat: 6, nickname: "Wicked Weber"))
         
-        classmates.append(Classmate(realName: "Cam Bradford", grade: 12, boat: 7, nickname: "Chef Cam"))
-        classmates.append(Classmate(realName: "Brennan Reinhard", grade: 10, boat: 7, nickname: "Breaking Brennan"))
+        AppData.classmates.append(Classmate(realName: "Cam Bradford", grade: 12, boat: 7, nickname: "Chef Cam"))
+        AppData.classmates.append(Classmate(realName: "Brennan Reinhard", grade: 10, boat: 7, nickname: "Breaking Brennan"))
         
-        classmates.append(Classmate(realName: "John Gariepy", grade: 12, boat: 1, nickname: "Jimmy John"))
-        classmates.append(Classmate(realName: "Jayden Sawyer", grade: 12, boat: 1, nickname: "Sleepy Sawyer"))
+        AppData.classmates.append(Classmate(realName: "John Gariepy", grade: 12, boat: 1, nickname: "Jimmy John"))
+        AppData.classmates.append(Classmate(realName: "Jayden Sawyer", grade: 12, boat: 1, nickname: "Sleepy Sawyer"))
         
-        classmates.append(Classmate(realName: "Sean McCain", grade: 11, boat: 2, nickname: "Boat Gone Sean"))
-        classmates.append(Classmate(realName: "Ryan Spencer", grade: 11, boat: 2, nickname: "Rainbow Ryan"))
-        classmates.append(Classmate(realName: "Logan Gough", grade: 11, boat: 2, nickname: "Googball Googan"))
-        classmates.append(Classmate(realName: "Michael Gruper", grade: 11, boat: 2, nickname: "Michael Michael Tricycle"))
+        AppData.classmates.append(Classmate(realName: "Sean McCain", grade: 11, boat: 2, nickname: "Boat Gone Sean"))
+        AppData.classmates.append(Classmate(realName: "Ryan Spencer", grade: 11, boat: 2, nickname: "Rainbow Ryan"))
+        AppData.classmates.append(Classmate(realName: "Logan Gough", grade: 11, boat: 2, nickname: "Googball Googan"))
+        AppData.classmates.append(Classmate(realName: "Michael Gruper", grade: 11, boat: 2, nickname: "Michael Michael Tricycle"))
         
-        classmates.append(Classmate(realName: "Daniel Meyer", grade: 11, boat: 4, nickname: "Bananiel"))
-        classmates.append(Classmate(realName: "Matthew Fitch", grade: 11, boat: 4, nickname: "Glitch Fitch"))
-        classmates.append(Classmate(realName: "Ryan Stark", grade: 11, boat: 4, nickname: "Rat Ryan"))
-        classmates.append(Classmate(realName: "Peter Mickle", grade: 11, boat: 4, nickname: "Peter Kickle"))
+        AppData.classmates.append(Classmate(realName: "Daniel Husby", grade: 11, boat: 4, nickname: "Bananiel"))
+        AppData.classmates.append(Classmate(realName: "Matthew Fitch", grade: 11, boat: 4, nickname: "Glitch Fitch"))
+        AppData.classmates.append(Classmate(realName: "Ryan Stark", grade: 11, boat: 4, nickname: "Rat Ryan"))
+        AppData.classmates.append(Classmate(realName: "Peter Mickle", grade: 11, boat: 4, nickname: "Peter Kickle"))
 
-
+        
+        for classmate in AppData.classmates{
+            var current = classmate.realName
+            AppData.classmatesNames.append(current)
+        }
 
 
 
