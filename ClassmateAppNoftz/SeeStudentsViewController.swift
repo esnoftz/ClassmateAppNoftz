f61df0a4be4c59add5ac9986b9e46b445531f7cf
Evangeline Noftz <esnoftz@gmail.com>
2024-10-04 14:03:26 -0500

    Finished MVP.

--- ClassmateAppNoftz/SeeStudentsViewController.swift
+++ ClassmateAppNoftz/SeeStudentsViewController.swift
@@ -19,9 +19,22 @@ class SeeStudentsViewController: UIViewController {
     
     @IBOutlet weak var gradeInput: UITextField!
     
+    
     @IBOutlet weak var boatInput: UITextField!
     
     
+    @IBOutlet weak var newStudentSubmitButton: UIButton!
+    
+    @IBOutlet weak var submitStudentWantEditButton: UIButton!
+    
+    @IBOutlet weak var wantEditTextField: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    @IBOutlet weak var editPersonSubmitButton: UIButton!
+    
+    var editPerson = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,12 +57,126 @@ class SeeStudentsViewController: UIViewController {
     
     @IBAction func sortStudentsAction(_ sender: UIButton) {
         
+        studentInfoTextView.text = ""
         AppData.classmatesNames.sort()
         for classmate in AppData.classmatesNames {
             studentInfoTextView.text += "\n\(classmate)"
         }
         
     }
+    
+    @IBAction func addStudentAction(_ sender: UIButton) {
+        
+        realNameInput.isHidden = false
+        nicknameInput.isHidden = false
+        gradeInput.isHidden = false
+        boatInput.isHidden = false
+        newStudentSubmitButton.isHidden = false
+        
+        
+        
+    }
+    
+    @IBAction func submitNewStudentAction(_ sender: UIButton) {
+        
+        AppData.classmates.append(Classmate(realName: realNameInput.text!, grade: Int(gradeInput.text!)!, boat: Int(boatInput.text!)!, nickname: nicknameInput.text!))
+        
+        AppData.classmatesNames.append(realNameInput.text!)
+        
+        realNameInput.isHidden = true
+        nicknameInput.isHidden = true
+        gradeInput.isHidden = true
+        boatInput.isHidden = true
+        newStudentSubmitButton.isHidden = true
+        
+        realNameInput.text = ""
+        nicknameInput.text = ""
+        gradeInput.text = ""
+        boatInput.text = ""
+        
+        
+    }
+    
+    
+    @IBAction func editStudentAction(_ sender: UIButton) {
+        
+        wantEditTextField.isHidden = false
+        submitStudentWantEditButton.isHidden = false
+        
+        
+    }
+    
+    @IBAction func studentWantEditAction(_ sender: UIButton) {
+        
+        var inArray = false
+        editPerson = wantEditTextField.text!
+        for i in AppData.classmatesNames {
+            if i == wantEditTextField.text {
+                inArray = true
+            }
+        }
+        
+        if inArray == true {
+            realNameInput.isHidden = false
+            nicknameInput.isHidden = false
+            gradeInput.isHidden = false
+            boatInput.isHidden = false
+            editPersonSubmitButton.isHidden = false
+        } else {
+            errorLabel.text = "This person does not exist!"
+        }
+        
+    }
+    
+    @IBAction func editPersonSubmitAction(_ sender: UIButton) {
+        var index = 0
+        var num = 0
+        for i in AppData.classmatesNames {
+            if i == editPerson {
+                index = num
+            }
+            num += 1
+        }
+        print(index)
+        
+        AppData.classmates[index].realName = realNameInput.text!
+        AppData.classmates[index].boat = Int(boatInput.text!)!
+        AppData.classmates[index].grade = Int(gradeInput.text!)!
+        AppData.classmates[index].nickname = nicknameInput.text!
+        
+        
+        realNameInput.text = ""
+        nicknameInput.text = ""
+        gradeInput.text = ""
+        boatInput.text = ""
+
+        
+        realNameInput.isHidden = true
+        nicknameInput.isHidden = true
+        gradeInput.isHidden = true
+        boatInput.isHidden = true
+        editPersonSubmitButton.isHidden = true
+        submitStudentWantEditButton.isHidden = true
+        wantEditTextField.isHidden = true
+        newStudentSubmitButton.isHidden = true
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
